dfa020522b61e0bbc254bbfee7a3eabff5b924ba
Maksymilian Bałdyga <s24862@pjwstk.edu.pl>
2023-04-28 14:45:57 +0200

    init

---
+++ CameraGroup.py
@@ -0,0 +1,18 @@
+import pygame
+
+
+class CameraGroup(pygame.sprite.Group):
+    def __init__(self):
+        super.__init__()
+        self.display_surface = pygame.display.get_surface()
+        # camera offset
+        self.offset = pygame.math.Vector2()
+        self.half_w = self.display_surface.get_size()[0] // 2
+        self.half_h = self.display_surface.get_size()[1] // 2
+
+    def center_target_camera(self, target):
+        self.offset.x = target.rect.centerx - self.half_w
+        self.offset.y = target.rect.centery - self.half_h
+
+    def custom_draw(self, player):
+        self.center_target_camera(player)
--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+from CameraGroup import CameraGroup
+from Player import Player
 
 # pygame setup
 pygame.init()
@@ -11,32 +13,40 @@ player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 width = screen.get_width()
 height = screen.get_height()
 
-bg = pygame.image.load("maxresdefault.jpg")
-bg = pygame.transform.scale(bg,(width,height))
+bg = pygame.image.load("Graphics/maxresdefault.jpg")
+bg = pygame.transform.scale(bg, (width, height))
 
+camera_group = CameraGroup
+player = Player((300,200), camera_group)
 while running:
-    screen.blit(bg,(0,0))
+    screen.blit(bg, (0, 0))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
 
+    player = pygame.image.load("Graphics/PlayerTest.png").convert_alpha()
 
-
-    pygame.draw.circle(screen, "red", player_pos, 40)
-    pygame.image.load("PlayerTest.png").convert()
-
-    keys = pygame.key.get_pressed()
-    if keys[pygame.K_w]:
-        player_pos.y -= 300 * dt
-    if keys[pygame.K_s]:
-        player_pos.y += 300 * dt
-    if keys[pygame.K_a]:
-        player_pos.x -= 300 * dt
-    if keys[pygame.K_d]:
-        player_pos.x += 300 * dt
-
+    # screen.fill("white")
+    screen.blit(player, (player_pos))
     pygame.display.flip()
+    # keys = pygame.key.get_pressed()
+    # if keys[pygame.K_w]:
+    #     if player_pos.y >= 0:
+    #         player_pos.y -= 300 * dt
+    # if keys[pygame.K_s]:
+    #     if player_pos.y <= 1050:
+    #         player_pos.y += 300 * dt
+    # if keys[pygame.K_a]:
+    #     if player_pos.x >= 0:
+    #         player_pos.x -= 300 * dt
+    # if keys[pygame.K_d]:
+    #     if player_pos.x <= 1850:
+    #         player_pos.x += 300 * dt
 
+    # camera_group.update()
+    camera_group.custom_draw(player)
+
+    pygame.display.update()
     dt = clock.tick(60) / 1000
 
 pygame.quit()
