6eb68a2b7706a977c4557ea87b97148f03b3b693
Maksymilian Bałdyga <s24862@pjwstk.edu.pl>
2023-04-28 19:15:29 +0200

    init

--- main.py
+++ main.py
@@ -9,20 +9,21 @@ screen = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
 clock = pygame.time.Clock()
 running = True
 dt = 0
-i=0
+i = 0
 
 # data for player posioton, width and height
 player_pos = pygame.Vector2(screen.get_width() / 2, screen.get_height() / 2)
 width = screen.get_width()
 height = screen.get_height()
 ogrepos = pygame.Vector2(700, 500)
+ogrepos1 = pygame.Vector2(1000, 500)
 # background setup
 background = pygame.image.load("Graphics/maxresdefault.jpg")
 background = pygame.transform.scale(background, (width, height))
 pygame.display.flip()
 # setup
-
-
+listForOgres=(ogre,ogre1)
+listForPositionsOfOgres=()
 pygame.display.flip()
 # starting loop
 while running:
@@ -35,14 +36,22 @@ while running:
             running = False
     player = Player(player_pos)
     screen.blit(player.image, player_pos)
-    if i==0:
+    if i == 0:
         ogre = Ogre(ogrepos, dt)
+        ogre1 = Ogre(ogrepos1, dt)
     else:
-        ogre=Ogre(updated,dt)
+        ogre = Ogre(updated, dt)
+        ogre1 = Ogre(updated1, dt)
+
     thread = threading.Thread(target=ogre.randomWalk())
     thread.start()
-    updated=ogre.pos
+    thread1 = threading.Thread(target=ogre1.randomWalk())
+    thread1.start()
+    updated = ogre.pos
+    updated1 = ogre1.pos
+
     screen.blit(ogre.image, ogre.pos)
+    screen.blit(ogre1.image, ogre1.pos)
     if (player.player_pos.x >= ogre.influenceSpherex.x) & (player.player_pos.x <= ogre.influenceSpherex.y) \
             & (player.player_pos.y >= ogre.influenceSpherey.x) & (player.player_pos.y <= ogre.influenceSpherey.y):
         running = False
@@ -58,8 +67,8 @@ while running:
         player_pos.x += 300 * dt
 
     pygame.display.flip()
-    print(ogre.pos)
+    print(ogre1.pos)
 
     dt = clock.tick(60) / 1000
-    i=i+1
+    i = i + 1
 pygame.quit()
