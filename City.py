97cd5280959ab59ce9cb5406ec5b4ae63406a9d6
Kacper Vu Manh <s24695@pjwstk.edu.pl>
2023-06-11 20:51:45 +0200

    uporzadkowany interface i pare zmian w city

--- City.py
+++ City.py
@@ -1,10 +1,23 @@
 import pygame
+
+import Interface
+
+
 class City():
-    def __init__(self,screen):
+    def __init__(self, screen):
         self.image = pygame.image.load("Graphics/cityImage.png").convert_alpha()
-    def enterCity(self,screen,heal,upgrade,leave,player,background):
-        width=screen.get_width()
-        height=screen.get_height()
+
+    def enterCity(self, screen, heal, upgrade, leave, player, background):
+        width = screen.get_width()
+        height = screen.get_height()
+        screen.blit(pygame.image.load("Graphics/PlayerInFight.png").convert_alpha(), (width * 0.45, height * 0.55))
+        font = pygame.font.Font("Fonts/zx_spectrum-7_bold.ttf", 30)
+        player_current_hp = font.render("Zycie: " + str(player.hp) + "/" + str(player.maxhp), True, "black")
+        screen.blit(player_current_hp, (width * 0.1, height * 0.75))
+        player_current_armour = font.render("Pancerz: " + str(player.armor), True, "black")
+        screen.blit(player_current_armour, (width * 0.4, height * 0.75))
+        player_current_gold = font.render("Zloto: " + str(player.gold), True, "black")
+        screen.blit(player_current_gold, (width * 0.75, height * 0.75))
         background = pygame.image.load("Graphics/city.png").convert_alpha()
         self.image = pygame.image.load("Graphics/blank.png").convert_alpha()
         background = pygame.transform.scale(background, (width, height))
@@ -13,9 +26,17 @@ class City():
         screen.blit(pygame.image.load("Graphics/upgrade.png").convert(), (width * 0.4, height * 0.8))
         screen.blit(pygame.image.load("Graphics/leave.png").convert(), (width * 0.75, height * 0.8))
         if heal:
-            player.hp = player.maxhp
+            if player.gold <= 0:
+                player.gold = 0
+                print("do zaimplementoowania")
+            elif player.hp < player.maxhp:
+                player.hp += 1
+                player.gold -= 2
+
         elif upgrade:
-            print("Do zaimplementowania")
-
-
-
+            if player.gold <= 0:
+                player.gold = 0
+                print("do zaimplementoowania")
+            else:
+                player.armor += 1
+                player.gold -= 2
