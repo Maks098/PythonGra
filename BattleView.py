e8ba2799a0fd1a0bbfa0aa231889dc5588e8bd92
KVuM <105286457+KVuM@users.noreply.github.com>
2023-06-18 21:15:23 +0200

    Update BattleView.py

--- BattleView.py
+++ BattleView.py
@@ -1,5 +1,5 @@
 import random
-
+import time
 import pygame
 
 
@@ -31,12 +31,12 @@ class BattleView:
         width = screen.get_width()
         height = screen.get_height()
 
-
         screen.blit(pygame.image.load("Graphics/attack.png").convert(), (width * 0.1, height * 0.8))
         screen.blit(pygame.image.load("Graphics/defend.png").convert(), (width * 0.4, height * 0.8))
         screen.blit(pygame.image.load("Graphics/runAttempt.png").convert(), (width * 0.75, height * 0.8))
         screen.blit(pygame.image.load("Graphics/PlayerInFight.png").convert_alpha(), (width * 0.15, height * 0.5))
         screen.blit(pygame.image.load("Graphics/OgreInFight.png").convert_alpha(), (width * 0.7, height * 0.4))
+
         pygame.draw.rect(screen, "red", (width * 0.13, height * 0.23, width * 0.15, height * 0.04))
         pygame.draw.rect(screen, "green", (width * 0.13, height * 0.23, width * 0.15 * playerRatio, height * 0.04))
         pygame.draw.rect(screen, "red", (width * 0.78, height * 0.23, width * 0.15, height * 0.04))
@@ -49,31 +49,29 @@ class BattleView:
         screen.blit(playerHpImg, (width * 0.15, height * 0.23))
         screen.blit(enemyHpImg, (width * 0.785, height * 0.23))
 
-
-
         if attack:
             playerChanceToStrike = random.randint(0, player.agility)
             enemyChanceToDodge = random.randint(0, enemy.agility)
 
             if playerChanceToStrike > enemyChanceToDodge:
                 enemy.getDamage(player.strength)
-                playerCommunication = font.render('Gracz zadał ' + str(player.strength - enemy.armor) + " obrażeń", True, "black")
-                screen.blit(playerCommunication, (width / 2, height * 0.1))
 
                 player_comm = "Gracz zadal " + str(player.strength - enemy.armor) + " pkt. obrazen"
                 comm_rect_center = (width / 2 * 0.85, height * 0.2)
 
                 self.print_message(screen, font, player_comm, comm_rect_center)
-                self.i = self.i + 1
+                self.i=self.i+1
+
             else:
                 player_comm_miss = "Gracz chybil"
                 comm_rect_center_miss = (width / 2 * 0.85, height * 0.2)
 
                 self.print_message(screen, font, player_comm_miss, comm_rect_center_miss)
-                self.i = self.i + 1
+                self.i=self.i+1
 
             playerChanceToDodge = random.randint(0, player.agility)
             enemyChanceToStrike = random.randint(0, enemy.agility)
+
             if enemyChanceToStrike > playerChanceToDodge:
                 player.getDamage(enemy.strength)
 
